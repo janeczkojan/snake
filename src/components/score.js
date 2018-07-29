02c1584281e35ad85bfeb3e3d51e26192c7a42cb
Jan Janeczko <janeczko.jan@hotmail.com>
2018-07-29 09:27:31 +0200

    add score component

--- src/components/score.js
+++ src/components/score.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import { number } from 'prop-types';
 
 
 class Score extends Component {
 
+  static propTypes = {
+    value: number.isRequired
+  }
+
+  static defaultProps = {
+    value: 0
+  }
+
+
   render() {
+    const { value } = this.props;
+
     return (
       <div>
-        <h3>0</h3>
+        <h3>{value}</h3>
       </div>
     );
   }
